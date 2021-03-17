3ed564ec8375e27177de541d06a4d44c9e58da9e
Brian James Celine <brianceline@gmail.com>
2021-03-17 13:23:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from app import Resident
+
+resident = Resident(name='Jennifer', age= '28', id=23)
+
+# resident.print_name()
+# resident.print_age()
+# resident.print_id()
+
+resident.title()
+
+# mro is method resolution order and this tells you the order of things getting called
+# print(Resident.__mro__)
+# This can also be caled directly
+print(Resident.mro())
