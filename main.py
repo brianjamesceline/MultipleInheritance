f836b70458b0c38c60393005a8435afdb9afef45
Brian James Celine <brianceline@gmail.com>
2021-03-24 13:09:58 +0000

    Slight Edit

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from app import Resident
 
 resident = Resident(name='Jennifer', age= '28', id=23)
 
-# resident.print_name()
-# resident.print_age()
-# resident.print_id()
+resident.print_name()
+resident.print_age()
+resident.print_id()
 
 resident.title()
 
